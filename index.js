6cd8c8ba383779ca4b0dfe992ec99e13297e1c6d
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-08-26 11:04:09 +0530

    Add prettier code format
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- index.js
+++ index.js
@@ -12,35 +12,41 @@ const os = require('os');
 
 let server;
 
-/** 
+/**
  * Accept CLI arguments
  */
 const yargs = require('yargs');
-const argv = yargs.options({
-	'port': {
+const argv = yargs
+	.options({
+		port: {
 			alias: 'p',
 			describe: 'the port to bind to on this machine',
 			default: process.env.PORT || 3000,
 			group: 'Configuration:',
 			type: 'number',
-			requiresArg: true
+			requiresArg: true,
 		},
-	'list': {
+		list: {
 			alias: 'L',
-			describe: 'a list file which has the paths to the files you want to share',
+			describe:
+				'a list file which has the paths to the files you want to share',
 			group: 'Configuration:',
 			type: 'string',
-			requiresArg: true
+			requiresArg: true,
 		},
-	'destination': {
+		destination: {
 			alias: 'd',
 			describe: 'folder where incoming files will be saved',
-			default: path.resolve(os.homedir(), 'Downloads', 'Broadcastem Received'),
+			default: path.resolve(
+				os.homedir(),
+				'Downloads',
+				'Broadcastem Received'
+			),
 			group: 'Configuration:',
 			type: 'string',
-			requiresArg: true
+			requiresArg: true,
 		},
-	'logging-level': {
+		'logging-level': {
 			alias: ['l', 'log'],
 			describe: `one of the following logging levels
 			0 - do not log anything
@@ -49,37 +55,42 @@ const argv = yargs.options({
 			default: 0,
 			group: 'Configuration:',
 			type: 'number',
-			requiresArg: true
+			requiresArg: true,
 		},
-	'core-version': {
+		'core-version': {
 			alias: 'c',
 			describe: `print version of the core module and exit`,
-			type: 'boolean'
-		}
+			type: 'boolean',
+		},
 	})
 	.check(argv => {
-		if(isNaN(argv.port))
-			throw new Error('Port should be a number');
-		else if(argv.port <= 0)
-			throw new Error('Port should be positive');
-		else if(argv.list && !fs.existsSync(argv.list))
+		if (isNaN(argv.port)) throw new Error('Port should be a number');
+		else if (argv.port <= 0) throw new Error('Port should be positive');
+		else if (argv.list && !fs.existsSync(argv.list))
 			throw new Error(`Specified file ${argv.list} doesnt exist`);
-		else if(isNaN(argv.loggingLevel))
+		else if (isNaN(argv.loggingLevel))
 			throw new Error('Logging level should be a number');
-		else if(argv.loggingLevel < 0 || argv.loggingLevel > 2)
+		else if (argv.loggingLevel < 0 || argv.loggingLevel > 2)
 			throw new Error('Invalid logging level');
-		else if(!argv._.every(path => fs.existsSync(path)))
-			throw new Error('All the files to be shared must be present on disk');
+		else if (!argv._.every(path => fs.existsSync(path)))
+			throw new Error(
+				'All the files to be shared must be present on disk'
+			);
 		return true;
 	})
-	.wrap(yargs.terminalWidth())
-	.argv;
+	.wrap(yargs.terminalWidth()).argv;
 // Since we are taking file paths as the remaining input, pass it on to files property for easy reading
 argv.files = argv._;
 argv.restart = true;
 
-if(argv.coreVersion) {
-	console.log(require(path.resolve(require.resolve('broadcastem-core'), '..', 'package.json')).version);
+if (argv.coreVersion) {
+	console.log(
+		require(path.resolve(
+			require.resolve('broadcastem-core'),
+			'..',
+			'package.json'
+		)).version
+	);
 	process.exit(0);
 }
 
@@ -91,13 +102,13 @@ function normalizePort(val) {
 	const port = parseInt(val, 10);
 
 	if (isNaN(port)) {
-	// named pipe
-	return val;
+		// named pipe
+		return val;
 	}
 
 	if (port >= 0) {
-	// port number
-	return port;
+		// port number
+		return port;
 	}
 
 	return false;
@@ -115,25 +126,23 @@ const port = normalizePort(argv.port);
 
 function onError(error) {
 	if (error.syscall !== 'listen') {
-	throw error;
+		throw error;
 	}
 
-	const bind = typeof port === 'string'
-	? 'Pipe ' + port
-	: 'Port ' + port;
+	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
 	// handle specific listen errors with friendly messages
 	switch (error.code) {
-	case 'EACCES':
-		console.error(bind + ' requires elevated privileges');
-		process.exit(1);
-		break;
-	case 'EADDRINUSE':
-		console.error(bind + ' is already in use');
-		process.exit(1);
-		break;
-	default:
-		throw error;
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
 	}
 }
 
@@ -143,19 +152,18 @@ function onError(error) {
 
 function onListening() {
 	const addr = server.address();
-	const bind = typeof addr === 'string'
-	? 'pipe ' + addr
-	: 'port ' + addr.port;
+	const bind =
+		typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 	console.log('Listening on ' + bind);
 }
 
 /**
  * Start Express app from the CLI flags
  */
- 
-broadcastemCore.init(argv)
-	.then((app) => {
 
+broadcastemCore
+	.init(argv)
+	.then(app => {
 		/**
 		 * Create HTTP server.
 		 */
@@ -169,27 +177,26 @@ broadcastemCore.init(argv)
 		server.listen(port);
 		server.on('error', onError);
 		server.on('listening', onListening);
-		
 	})
-	.catch((err) => {
+	.catch(err => {
 		console.error(err);
 		process.exit(1);
 	})
 	.finally(() => {
 		if (process.platform === 'win32') {
 			const rl = require('readline').createInterface({
-			input: process.stdin,
-			output: process.stdout
+				input: process.stdin,
+				output: process.stdout,
 			});
 
 			rl.on('SIGINT', function () {
-			process.emit('SIGINT');
+				process.emit('SIGINT');
 			});
 		}
 
 		process.on('SIGINT', function () {
-			if(server.listening) server.close();
-			console.log('Shutting down server')
+			if (server.listening) server.close();
+			console.log('Shutting down server');
 			process.exit();
 		});
 	});
